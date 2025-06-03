2a5980a594b4d3d11f944106599a938031a3c1de
Roxilius <fajrikhairan08@gmail.com>
2025-06-03 22:11:07 +0700

    .

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -6,61 +6,35 @@ import { FaNodeJs } from "react-icons/fa";
 import { motion } from "framer-motion";
 import { FiFramer } from "react-icons/fi";
 
-const variants = (duration) => ({
-  initial: { opacity: 0, y: -10 },
+const variants = (duration, delay) => ({
+  initial: { opacity: 0, y: 0 },
   animate: {
     opacity: 1,
-    y: [10, -10],
+    y: [-5, 5, -5],
     transition: {
-      opacity: { duration: 0.3 },
+      opacity: { duration: 0.5, delay },
       y: {
-        duration: duration,
+        duration,
+        delay,
         ease: "easeInOut",
         repeat: Infinity,
       },
     },
   },
+
 });
 
 const techs = [
-  {
-    icon: <BiLogoSpringBoot />,
-    color: "#6DB33F",
-    duration: 1.5,
-  },
-  { icon: <SiThymeleaf />, color: "#005F0F", duration: 2 },
-  { icon: <FaNodeJs />, color: "#339933", duration: 1.5 },
-  {
-    icon: <RiReactjsLine />,
-    color: "#61DAFB",
-    duration: 2,
-  },
-  {
-    icon: <RiTailwindCssFill />,
-    color: "#06B6D4",
-    duration: 1.5,
-  },
-  {
-    icon: <FiFramer />,
-    color: "#0055FF",
-    duration: 2,
-  },
-  {
-    icon: <SiTypescript />,
-    color: "#3178C6",
-    duration: 1.5,
-  },
-  {
-    icon: <RiVercelLine />,
-    color: "#ffffff",
-    duration: 2,
-  },
-  {
-    icon: <BiLogoPostgresql />,
-    color: "#336791",
-    duration: 1.5,
-  },
-  { icon: <SiMysql />, color: "#00758F", duration: 2 },
+  { icon: <BiLogoSpringBoot />, color: "#6DB33F", duration: 2 },
+  { icon: <SiThymeleaf />, color: "#005F0F", duration: 2.2 },
+  { icon: <FaNodeJs />, color: "#339933", duration: 1.8 },
+  { icon: <RiReactjsLine />, color: "#61DAFB", duration: 2 },
+  { icon: <RiTailwindCssFill />, color: "#06B6D4", duration: 2.4 },
+  { icon: <FiFramer />, color: "#0055FF", duration: 1.7 },
+  { icon: <SiTypescript />, color: "#3178C6", duration: 2.3 },
+  { icon: <RiVercelLine />, color: "#ffffff", duration: 2 },
+  { icon: <BiLogoPostgresql />, color: "#336791", duration: 1.9 },
+  { icon: <SiMysql />, color: "#00758F", duration: 2.1 },
 ];
 
 const TechStack = () => {
@@ -74,7 +48,7 @@ const TechStack = () => {
         {techs.map((tech, index) => (
           <motion.div
             key={index}
-            variants={variants(tech.duration)}
+            variants={variants(tech.duration, index * 0.3)}
             initial="initial"
             animate="animate"
             className="rounded-4xl text-7xl border-4 border-neutral-800 p-3 shadow-md  hover:scale-105 duration-300"
