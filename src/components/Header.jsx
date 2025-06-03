4691634af92348d1a104deb982bb9ac0d77e53d9
Roxilius <fajrikhairan08@gmail.com>
2025-06-03 20:03:21 +0700

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import { motion } from "framer-motion";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  // const navigate = useNavigate();
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
@@ -12,9 +14,15 @@ const Header = () => {
         {/* <img className="mx-2 w-10" src="" alt="logo" /> */}
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin className="text-blue-400" />
-        <FaGithub className="text-slate-400" />
-        <FaInstagram className="text-red-400" />
+        <a href="https://www.linkedin.com/in/fajri-khairan/">
+          <FaLinkedin className="text-blue-400" />
+        </a>
+        <a href="https://github.com/Roxilius">
+          <FaGithub className="text-slate-400" />
+        </a>
+        <a href="https://www.instagram.com/roxilius___/">
+          <FaInstagram className="text-red-400" />
+        </a>
       </div>
     </nav>
   );
